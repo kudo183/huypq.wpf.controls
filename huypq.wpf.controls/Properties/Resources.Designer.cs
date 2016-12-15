9dc7ea44f9819e4d3f19da16b4e5038591870540
gaucon <kudo183@gmail.com>
2016-12-15 18:01:22 +0700

    - change target from 4.5.2 to 4.5

--- huypq.wpf.controls/Properties/Resources.Designer.cs
+++ huypq.wpf.controls/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace huypq.wpf.controls.Properties {
+    using System;
     
     
     /// <summary>
@@ -37,7 +38,7 @@ namespace huypq.wpf.controls.Properties {
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if ((resourceMan == null)) {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("huypq.wpf.controls.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
