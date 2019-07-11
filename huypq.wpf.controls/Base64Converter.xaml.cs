40327272849274b1c7e00a41241c60f216ca3fbe
i9400 <kudo183@gmail.com>
2019-07-11 11:00:12 +0700

    - add Bytes format, number converter.

--- huypq.wpf.controls/Base64Converter.xaml.cs
+++ huypq.wpf.controls/Base64Converter.xaml.cs
@@ -94,5 +94,101 @@ namespace huypq.wpf.controls
         {
             Clipboard.SetText(txtBase64.Text);
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (txtData.Text.StartsWith("0x") == true)
+            {
+                txtData.Text = txtData.Text.Replace("0x", "").Replace(" ", "").Replace(",", "");
+            }
+            else
+            {
+                var temp = txtData.Text.ToArray();
+                var sb = new StringBuilder();
+                for (int i = 0; i < temp.Length; i += 2)
+                {
+                    if (i > 0)
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.Append("0x");
+                    sb.Append(temp[i]);
+                    sb.Append(temp[i + 1]);
+                }
+                txtData.Text = sb.ToString();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (txtData.Text.Contains(" ") == true)
+            {
+                txtData.Text = txtData.Text.Replace(" ", "");
+            }
+            else
+            {
+                var temp = txtData.Text.ToArray();
+                var sb = new StringBuilder();
+                for (int i = 0; i < temp.Length; i += 2)
+                {
+                    if (i > 0)
+                    {
+                        sb.Append(" ");
+                    }
+                    sb.Append(temp[i]);
+                    sb.Append(temp[i + 1]);
+                }
+                txtData.Text = sb.ToString();
+            }
+        }
+
+        private void TxtHex_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TxtDec_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var number = int.Parse(txtDec.Text);
+            txtHex.Text = Convert.ToString(number, 16);
+            txtBin.Text = Convert.ToString(number, 2);
+        }
+
+        private void TxtBin_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            var number = int.Parse(txtBin.Text);
+            txtDec.Text = Convert.ToString(number, 10);
+            txtHex.Text = Convert.ToString(number, 16);
+        }
+
+        private void TxtHex_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var number = Convert.ToInt32(txtHex.Text, 16);
+                txtDec.Text = Convert.ToString(number, 10);
+                txtBin.Text = Convert.ToString(number, 2);
+            }
+        }
+
+        private void TxtDec_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var number = Convert.ToInt32(txtDec.Text, 10);
+                txtHex.Text = Convert.ToString(number, 16);
+                txtBin.Text = Convert.ToString(number, 2);
+            }
+        }
+
+        private void TxtBin_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var number = Convert.ToInt32(txtBin.Text, 2);
+                txtHex.Text = Convert.ToString(number, 16);
+                txtDec.Text = Convert.ToString(number, 10);
+            }
+        }
     }
 }
